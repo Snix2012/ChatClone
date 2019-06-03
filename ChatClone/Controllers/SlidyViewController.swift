2dd1518a7853b553c835b2b42bff0e8a99e295a0
Claire Roughan <claire@fsbtech.com>
2019-06-03 17:33:40 +0100

    Playing with drawing code

--- ChatClone/Controllers/SlidyViewController.swift
+++ ChatClone/Controllers/SlidyViewController.swift
@@ -13,6 +13,10 @@ class SlidyViewController: UIViewController {
 
     @IBOutlet weak var labelToSlideIn: UILabel!
     
+    var borderWidth : CGFloat = 4 // Should be less or equal to the `radius` property
+    var radius : CGFloat = 10
+    var triangleHeight : CGFloat = 15
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,21 +30,23 @@ class SlidyViewController: UIViewController {
         view.addGestureRecognizer(leftSwipe)
         view.addGestureRecognizer(rightSwipe)
         
-        drawSquare()
+//        drawSquare()
+         drawCircle()
+//        drawSpeachBubble()
+//
         
-        drawCircle()
+        drawAdminSpeachBubble()
+        drawUserSpeachBubble()
         
         
-        // Only round the top left and right corners of the rectangle.// Only round the top left and right corners of the rectangle.
-        let rectanglePath = UIBezierPath.init(roundedRect: CGRect(x: 170, y: 70, width: 60, height: 40), byRoundingCorners: [.topLeft, .bottomRight], cornerRadii: CGSize(width: 4.0, height: 4.0))
-        
-        let rectangle = CAShapeLayer()
-        
-        // Apply the squarePath to our layer
-        rectangle.path = rectanglePath.cgPath
-        rectangle.fillColor = UIColor.green.cgColor
-        self.view.layer.addSublayer(rectangle)
-
+        //Example usage:
+        let bubbleLayer = CAShapeLayer()
+        bubbleLayer.path = bubblePathForContentSize(contentSize:CGSize(width: 100, height: 50)).cgPath
+        bubbleLayer.fillColor = (UIColor(red: 54/255, green: 172/255, blue: 224/255, alpha: 0.8)).cgColor
+        bubbleLayer.strokeColor = UIColor.darkGray.cgColor
+        bubbleLayer.lineWidth = 1.0
+        bubbleLayer.position = CGPoint.zero
+        self.view.layer.addSublayer(bubbleLayer)
         
     }
     
@@ -61,25 +67,90 @@ class SlidyViewController: UIViewController {
         }
     }
     
-    fileprivate func drawCircle() {
+    fileprivate func drawUserSpeachBubble() {
+        // Round the corners of the rectangle
+        let rectanglePath = UIBezierPath.init(roundedRect: CGRect(x: 240, y: 120, width: 70, height: 40), byRoundingCorners: [.topLeft, .topRight, .bottomLeft ], cornerRadii: CGSize(width: 12.0, height: 12.0))
         
-//       let circlePath = UIBezierPath()
-//        circlePath.addCurve(to: <#T##CGPoint#>, controlPoint1: <#T##CGPoint#>, controlPoint2: <#T##CGPoint#>)
-       
+        let rectangle = CAShapeLayer()
         
+        // Apply the squarePath to our layer
+        rectangle.path = rectanglePath.cgPath
+        rectangle.fillColor = (UIColor(red: 253/255, green: 229/255, blue: 72/255, alpha: 0.8)).cgColor
+        self.view.layer.addSublayer(rectangle)
     }
+    
+    fileprivate func drawAdminSpeachBubble() {
+        // Round the corners of the rectangle
+        let rectanglePath = UIBezierPath.init(roundedRect: CGRect(x: 30, y: 170, width: 70, height: 40), byRoundingCorners: [.topLeft, .topRight, .bottomRight ], cornerRadii: CGSize(width: 12.0, height: 12.0))
+        
+        let rectangle = CAShapeLayer()
+        
+        // Apply the squarePath to our layer
+        rectangle.path = rectanglePath.cgPath
+        rectangle.fillColor = (UIColor(red: 152/255, green: 208/255, blue: 77/255, alpha: 0.8)).cgColor
+        self.view.layer.addSublayer(rectangle)
+    }
+    
+    fileprivate func drawSpeachBubble() {
+        
+        let ovalPath = UIBezierPath.init(ovalIn: CGRect(x:40, y:100, width:150, height:100))
+        UIGraphicsBeginImageContext(CGSize(width: 250, height: 200));
+        
+        //this gets the graphic context
+        let context = UIGraphicsGetCurrentContext()
+        
+        //you can stroke and/or fill
+        context!.setStrokeColor(UIColor.black.cgColor)
+        context!.setFillColor(UIColor.lightGray.cgColor)
+        ovalPath.fill()
+        ovalPath.stroke()
+        
+        //now get the image from the context
+        let bezierImage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        
+        let ovalImageView = UIImageView.init(image:  bezierImage)
+        self.view.addSubview(ovalImageView)
+
+//        let aRect = CGRect(x: 2.0, y: 2.0, width: (self.view.bounds.size.width * 0.95), height: (self.view.bounds.size.width * 0.60))
+//        UIGraphicsBeginImageContext(aRect)
+//        ctx!.setStrokeColor(UIColor.black.cgColor)
+//        ctx!.setLineWidth(CGFloat(2.0))
+//        ctx!.fillEllipse(in:aRect)
+//        ctx!.strokeEllipse(in: aRect)
+//
+//        ctx?.beginPath()
+//        ctx?.move(to: CGPoint(x:self.view.bounds.size.width * 0.10, y:self.view.bounds.size.width * 0.48))
+//        ctx!.addLine(to: CGPoint(x:3.0, y:self.view.bounds.size.height * 0.80))
+//        ctx!.addLine(to: CGPoint(x:20.0, y:self.view.bounds.size.height * 0.70))
+//        ctx!.closePath()
+//        ctx!.fillPath()
+//
+//        ctx!.beginPath()
+//        ctx!.move(to: CGPoint(x:self.view.bounds.size.width * 0.10, y:self.view.bounds.size.width * 0.48))
+//        ctx!.addLine(to: CGPoint(x: 3.0, y:self.view.bounds.size.height * 0.80))
+//        ctx!.strokePath()
+//
+//        ctx!.beginPath()
+//        ctx!.move(to: CGPoint(x:3.0, y:self.view.bounds.size.height * 0.80))
+//        ctx!.addLine(to: CGPoint(x:20.0, y:self.view.bounds.size.height * 0.70))
+//        ctx!.strokePath()
+    }
+    
     
     fileprivate func drawSquare() {
         //draw the shape path in coordinates
         let squarePath = UIBezierPath()
         
         // Move to start coordinates
-        squarePath.move(to: CGPoint(x: 300, y: 300))
+        squarePath.move(to: CGPoint(x: 100, y: 400))
         
         // Do drawing lines
-        squarePath.addLine(to: CGPoint(x: 200, y: 100))
+        squarePath.addLine(to: CGPoint(x: 100, y: 400))
         squarePath.addLine(to: CGPoint(x: 200, y: 200))
         squarePath.addLine(to: CGPoint(x: 100, y: 200))
+        squarePath.addLine(to: CGPoint(x: 100, y: 100))
         
         // close drawing
         squarePath.close()
@@ -93,21 +164,85 @@ class SlidyViewController: UIViewController {
         self.view.layer.addSublayer(square)
     }
    
+    
+    fileprivate func bubblePathForContentSize(contentSize: CGSize) -> UIBezierPath {
+        let rect = CGRect(origin:CGPoint(x: 20, y: 250), size: CGSize(width: contentSize.width, height: contentSize.height)).offsetBy(dx: radius, dy: radius + triangleHeight)
+        let path = UIBezierPath();
+        let radius2 = radius - borderWidth / 2 // Radius adjasted for the border width
+        
+        path.move(to: CGPoint(x: rect.maxX - triangleHeight * 2, y: rect.minY - radius2))
+        path.addLine(to: CGPoint(x: rect.maxX - triangleHeight, y: rect.minY - radius2 - triangleHeight))
+        path.addArc(withCenter: CGPoint(x: rect.maxX, y: rect.minY),
+                    radius: radius2,
+                    startAngle: CGFloat(-(Double.pi/2)), endAngle: 0, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: rect.maxX, y: rect.maxY),
+                    radius: radius2,
+                    startAngle: 0, endAngle: CGFloat(Double.pi/2), clockwise: true)
+        path.addArc(withCenter: CGPoint(x: rect.minX, y: rect.maxY),
+                    radius: radius2,
+                    startAngle: CGFloat(Double.pi/2),endAngle: CGFloat(Double.pi), clockwise: true)
+        path.addArc(withCenter: CGPoint(x: rect.minX, y: rect.minY),
+                    radius: radius2,
+                    startAngle: CGFloat(Double.pi), endAngle: CGFloat(-(Double.pi/2)), clockwise: true)
+        path.close()
+        return path
+    }
 
+    
+    fileprivate func arrow(from start: CGPoint, to end: CGPoint, tailWidth: CGFloat, headWidth: CGFloat, headLength: CGFloat) -> CGPath {
+        let length = hypot(end.x - start.x, end.y - start.y)
+        let tailLength = length - headLength
+        
+        func p(_ x: CGFloat, _ y: CGFloat) -> CGPoint { return CGPoint(x: x, y: y) }
+        var points: [CGPoint] = [
+            p(0, tailWidth / 2),
+            p(tailLength, tailWidth / 2),
+            p(tailLength, headWidth / 2),
+            p(length, 0),
+            p(tailLength, -headWidth / 2),
+            p(tailLength, -tailWidth / 2),
+            p(0, -tailWidth / 2)
+        ]
+        
+        let cosine = (end.x - start.x) / length
+        let sine = (end.y - start.y) / length
+        var transform = CGAffineTransform(a: cosine, b: sine, c: -sine, d: cosine, tx: start.x, ty: start.y)
+        
+        let path = CGMutablePath()
+        path.addLines(between: points, transform: transform)
+        path.closeSubpath()
+        
+        return (path) as CGPath
+    }
+
+    
+    fileprivate func drawCircle() {
+        let theArrow = CAShapeLayer()
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 212, height: 212))
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.green.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            let rectangle = CGRect(x: self.view.frame.size.width / 2, y: self.view.frame.size.height / 2 , width: 212, height: 212)
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+           
+//            theArrow.path = arrow(from: CGPoint(x: 220, y: 280), to: CGPoint(x: 220, y: 260),
+//                                  tailWidth: 2, headWidth: 12, headLength: 8)
+//
+//            ctx.cgContext.addPath(theArrow.path!)
+
+        }
+        self.view.addSubview(UIImageView.init(image: img))
+        
+        
+    }
 }
 
-//
-//func circlePathWithCenter(center: CGPoint, radius: CGFloat) -> UIBezierPath {
-//    let circlePath = UIBezierPath()
-//    circlePath.addArc(withCenter: center, radius: radius, startAngle: -CGFloat(M_PI), endAngle: -CGFloat(M_PI/2), clockwise: true)
-//    circlePath.addArc(withCenter: center, radius: radius, startAngle: -CGFloat(M_PI/2), endAngle: 0, clockwise: true)
-//    circlePath.addArc(withCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(M_PI/2), clockwise: true)
-//    circlePath.addArc(withCenter: center, radius: radius, startAngle: CGFloat(M_PI/2), endAngle: CGFloat(M_PI), clockwise: true)
-//    circlePath.close()
-//    return circlePath
-//}
-//
-//
+
+
 //let circle = CAShapeLayer()
 //circle.path = circlePathWithCenter(center: CGPoint(x: 200,y: 400), radius: 50).cgPath
 //circle.fillColor = UIColor.blue.cgColor
